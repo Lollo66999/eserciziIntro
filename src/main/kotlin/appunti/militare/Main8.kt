8a56ced72e84c646312ab4badcf2366731bbd0c5
Lollo66999 <76514137+Lollo66999@users.noreply.github.com>
2020-12-28 13:55:01 +0100

    Create Main8.kt

---
+++ src/main/kotlin/appunti/militare/Main8.kt
@@ -0,0 +1,9 @@
+package appunti.militare
+
+fun main() {
+    val S1 = `Soldato numero 1`("Giorgio", "Pallotola", "cecchino")
+    val S2 = `Soldato numero 2`("Federico", "Missile", "lanciarazzi")
+
+    print(S1.descrizione)
+    print(S2.descrizione)
+}
