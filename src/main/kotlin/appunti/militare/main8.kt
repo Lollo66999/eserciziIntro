77094456d9413a52e58d041ffa8ff80f72209834
Lollo66999 <76514137+Lollo66999@users.noreply.github.com>
2021-01-03 19:25:54 +0100

    Militare

---
+++ src/main/kotlin/appunti/militare/main8.kt
@@ -0,0 +1,9 @@
+package appunti.militare
+
+fun main() {
+    val S1 = `Soldato numero 1`("Giorgio", "Pallotola", "maschio", "cecchino")
+    val S2 = `Soldato numero 2`("Federico", "Missile", "maschio", "lanciarazzi")
+
+    println(S1.descrizione)
+    println(S2.descrizione)
+}
