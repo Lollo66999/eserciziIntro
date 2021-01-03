b937e29f2674a424e56b937a528e2f4cb564966e
Lollo66999 <76514137+Lollo66999@users.noreply.github.com>
2021-01-03 19:27:56 +0100

    Delete Militare.kt

--- src/main/kotlin/appunti/militare/Militare.kt
+++
@@ -1,6 +0,0 @@
-package appunti.militare
-
-abstract class Militare(var nome: String, var cognome: String, var arma: String) {
-    val descrizione: String
-        get() = "$nome $cognome, arma principlae $arma"
-}
